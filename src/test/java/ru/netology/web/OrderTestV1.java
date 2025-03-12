270240de5e40988121be8953468d043bad87636b
Tamara Sibileva <sibilevato@gmail.com>
2025-03-12 22:10:58 +1200

    Test failed

---
+++ src/test/java/ru/netology/web/OrderTestV1.java
@@ -0,0 +1,55 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class OrderTestV1 {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldSendForm() {
+        WebElement formElement = driver.findElement(By.cssSelector("form"));
+        formElement.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Зайцев Алексей");
+        formElement.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+71234567899");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("[role='button']")).click();
+        WebElement actual = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(actual.isDisplayed());
+        assertEquals("Ваша успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actual.getText().trim());
+    }
+}
